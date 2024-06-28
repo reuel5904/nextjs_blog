40f27675c38b01a6220c05469b67981edc7ea1ea
reuel5904 <reuelpalancas@gmail.com>
2024-06-27 23:10:42 -0400

    Create drafts page

--- app/drafts/page.tsx
+++ app/drafts/page.tsx
@@ -1,7 +1,25 @@
-export default function DraftsPage() {
+import Post from "@/components/Post";
+import prisma from "@/lib/prisma";
+import styles from './Drafts.module.css'
+
+export default async function DraftsPage() {
+  const drafts = await prisma.post.findMany({
+    where: { 
+      published: false,
+    },
+    include: {
+      author: true,
+    },
+  });
+
   return (
     <main>
       <h1>Drafts</h1>
+      {drafts.map(post => (
+        <div key={post.id} className={styles.draft}>
+          <Post post={post} />
+        </div>
+      ))}
     </main>
   )
 }
